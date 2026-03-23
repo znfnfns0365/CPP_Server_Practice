8ce9dfb05009de440185405c96b97546d4d9442d
김동헌 <znfnfns0365c@gmail.com>
2026-03-23 15:22:56 +0900

    WSAEventSelect Model

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -12,64 +12,83 @@ void HandleError(const char* funcName) {
 }
 
 int main() {
+	this_thread::sleep_for(1s);
+
 	WSADATA wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
-	if (clientSocket == INVALID_SOCKET) {
-		HandleError("socket");
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
 		return 0;
-	}
 
-	SOCKADDR_IN serverAddr;	 // IPv4 주소 구조체
+	u_long on = 1;
+	if (::ioctlsocket(clientSocket, FIONBIO, &on) == SOCKET_ERROR)
+		return 0;
+
+	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = ::htons(7777);
+	serverAddr.sin_port = htons(7777);
 
-	// Connected UDP
-	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-
+	// Connect to Server
 	while (true) {
-		char sendBuffer[100] = "Hello Server";
-		for (int32 i = 0; i < 10; i++) {
-			// Unconnected UDP
-			// int32 sendLen =
-			// 	::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+			// 원래 connect 될 때까지 블록인데, 논블록 설정 중이라 넘어옴
+			int WSA = ::WSAGetLastError();
+			if (WSA == WSAEWOULDBLOCK || WSA == WSAEALREADY)
+				continue;
 
-			// Connected UDP
-			int32 sendLen = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+			// 이미 연결되어 있음
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
 
-			if (sendLen == SOCKET_ERROR) {
-				HandleError("sendto");
-				return 0;
+			// Error
+			cout << WSA << endl;
+			// return 0;
+		}
+	}
+
+	cout << "Connected to Server!" << endl;
+
+	char sendBuffer[100] = "Hello Server!";
+	while (true) {
+		int sendLen = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+		if (sendLen == SOCKET_ERROR) {
+			// 원래 send 될 때까지 블록인데, 논블록 설정 중이라 넘어옴
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			// Error
+			break;
+		}
+
+		cout << "Send Data! Len =" << sizeof(sendBuffer) << endl;
+
+		while (true) {
+			char recvBuffer[1000];
+			int recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR) {
+				// 원래 recv 될 때까지 블록인데, 논블록 설정 중이라 넘어옴
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+
+				// Error
+				break;
+			} else if (recvLen == 0) {
+				// 서버가 연결을 끊음
+				cout << "Client Disconnected!" << endl;
+				break;
 			}
-			cout << "Send Data! Data =" << sendBuffer << endl;
-			cout << "Send Data! Len =" << sendLen << endl;
+
+			cout << "Recv Data! Len =" << recvLen << endl;
+			break;
 		}
-
-		SOCKADDR_IN recvAddr;
-		::memset(&recvAddr, 0, sizeof(recvAddr));
-		int32 recvAddrLen = sizeof(recvAddr);
-		char recvBuffer[100];
-		// Unconnected UDP
-		// int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&recvAddr,
-		// &recvAddrLen);
-
-		// Connected UDP
-		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-
-		if (recvLen <= 0) {
-			HandleError("recvfrom");
-			return 0;
-		}
-
-		cout << "Recv Data! Data =" << recvBuffer << endl;
-		cout << "Recv Data! Len =" << recvLen << endl;
 
 		this_thread::sleep_for(1s);
 	}
+
 	// ---------------------------------------
 
 	// 소켓 리소스 반환
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,6 +7,7 @@
 #include <future>
 #include "ThreadManager.h"
 
+#include <urlmon.h>
 #include <winSock2.h>
 #include <mswsock.h>
 #include <WS2tcpip.h>
@@ -17,51 +18,141 @@ void HandleError(const char* funcName) {
 	cout << "Causon Func: " << funcName << endl << "socket error : " << errCode << endl;
 }
 
+const int32 BUFFER_SIZE = 1000;
+
+struct Session {
+	SOCKET socket;
+	char recvBuffer[BUFFER_SIZE] = {};
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
+};
+
 int main() {
 	WSADATA wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
-	if (serverSocket == INVALID_SOCKET) {
-		HandleError("socket");
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
 		return 0;
-	}
 
-	SOCKADDR_IN serverAddr;	 // IPv4 주소 구조체
+	u_long on = 1;
+	if (::ioctlsocket(listenSocket, FIONBIO, &on) == SOCKET_ERROR)
+		return 0;
+
+	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-	serverAddr.sin_port = ::htons(7777);
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(7777);
 
-	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-		HandleError("bind");
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 		return 0;
-	}
+
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+		return 0;
+
+	cout << "Accept" << endl;
+
+	// WSAEVENT, Session 벡터를 만들어 각 소켓과 연동된 이벤트 객체를 관리
+	vector<WSAEVENT> wsaEvents;
+	vector<Session> sessions;
+	sessions.reserve(100);	// 불필요한 복사를 막기 위해 미리 공간을 할당
+
+	WSAEVENT listenEvent = WSACreateEvent();
+	wsaEvents.push_back(listenEvent);
+	sessions.push_back(Session{listenSocket});
+	if (::WSAEventSelect(listenSocket, listenEvent, FD_ACCEPT | FD_CLOSE) == SOCKET_ERROR)
+		return 0;
 
 	while (true) {
-		SOCKADDR_IN clientAddr;
-		::memset(&clientAddr, 0, sizeof(clientAddr));
-		int32 clientAddrLen = sizeof(clientAddr);
-		char recvBuffer[100];
+		int32 index = WSAWaitForMultipleEvents(wsaEvents.size(), &wsaEvents[0], FALSE, WSA_INFINITE, FALSE);
+		if (index == WSA_WAIT_FAILED)
+			continue;
 
-		this_thread::sleep_for(1s);
-		int32 recvLen =
-			::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&clientAddr, &clientAddrLen);
+		// WSAWaitForMultipleEvents의 return value에 WSA_WAIT_EVENT_0가 더해져서 오기 때문에 빼줌
+		index -= WSA_WAIT_EVENT_0;
 
-		if (recvLen == SOCKET_ERROR) {
-			HandleError("recvfrom");
-			return 0;  // 원래는 끝내는게 아니라 해당 클라이언트를 제외하고 다시 받는 작업을 해야함
+		// ::WSAResetEvent(wsaEvents[index]);
+		// WSAEnumNetworkEvents의 2번째 인자에 wasEvents[index]를 넘겨주면, 자동으로로 이벤트 객체를 non-signaled 상태로
+		// 리셋하기 때문에 안 해도 됨
+
+		WSANETWORKEVENTS networkEvents;
+		if (::WSAEnumNetworkEvents(sessions[index].socket, wsaEvents[index], &networkEvents) == SOCKET_ERROR)
+			continue;
+
+		// Listener Socket 체크
+		if (networkEvents.lNetworkEvents & FD_ACCEPT) {
+			// Error-Check
+			if (networkEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
+				continue;
+
+			SOCKADDR_IN clientAddr;
+			int32 addrLen = sizeof(clientAddr);
+			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+			if (clientSocket != INVALID_SOCKET) {
+				cout << "Client Connected" << endl;
+
+				WSAEVENT clientEvent = WSACreateEvent();
+				wsaEvents.push_back(clientEvent);
+				sessions.push_back(Session{clientSocket});
+				if (::WSAEventSelect(clientSocket, clientEvent, FD_READ | FD_WRITE | FD_CLOSE) == SOCKET_ERROR)
+					return 0;
+			}
 		}
 
-		cout << "Recv Data! Data=" << recvBuffer << endl;
-		cout << "Recv Data Length=" << recvLen << endl;
+		// Client Session 소켓 체크
+		if (networkEvents.lNetworkEvents & FD_READ || networkEvents.lNetworkEvents & FD_WRITE) {
+			// Error-Check
+			if ((networkEvents.lNetworkEvents & FD_READ) && (networkEvents.iErrorCode[FD_READ_BIT] != 0))
+				continue;
 
-		int32 errorCode =
-			::sendto(serverSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&clientAddr, clientAddrLen);
-		if (errorCode == SOCKET_ERROR) {
-			HandleError("sendto");
-			return 0;
+			// Error-Check
+			if ((networkEvents.lNetworkEvents & FD_WRITE) && (networkEvents.iErrorCode[FD_WRITE_BIT] != 0))
+				continue;
+
+			Session& s = sessions[index];
+
+			// Read
+			if (s.recvBytes == 0) {
+				int32 recvLen = ::recv(s.socket, s.recvBuffer, BUFFER_SIZE, 0);
+
+				// 드물게 WSAEWOULDBLOCK 오류가 뜰 수 있음
+				// Socket Err 발생했을 때, GetLastError가 WOULDBLOCK이 아니면 에러 처리
+				if (recvLen == SOCKET_ERROR) {
+					if (::WSAGetLastError() != WSAEWOULDBLOCK)
+						// TODO: Remove Session
+						return 0;
+					continue;
+				}
+
+				s.recvBytes = recvLen;
+				cout << "Recv Data! Len =" << recvLen << endl;
+			}
+			if (s.recvBytes > s.sendBytes) {
+				int32 sendLen = ::send(s.socket, &s.recvBuffer[s.sendBytes], s.recvBytes - s.sendBytes, 0);
+				if (sendLen == SOCKET_ERROR) {
+					if (::WSAGetLastError() != WSAEWOULDBLOCK) {
+						// TODO: Remove Session
+						return 0;
+					}
+					continue;
+				}
+
+				s.sendBytes += sendLen;
+				if (s.recvBytes == s.sendBytes) {
+					s.sendBytes = 0;
+					s.recvBytes = 0;
+				}
+
+				cout << "Send Data! Len =" << sendLen << endl;
+			}
+		}
+
+		// FD_CLOSE 처리
+		if (networkEvents.lNetworkEvents & FD_CLOSE) {
+			// TODO: Remove Session
+			continue;
 		}
 	}
 
